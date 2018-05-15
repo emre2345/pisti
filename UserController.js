7a615daa6441c2c414d60257ff53dc97b079858e
Emre Solak <s.emresolak@dreamharvesters.com>
2018-05-15 11:54:41 +0300

    rev30

--- UserController.js
+++ UserController.js
@@ -1,4 +1,4 @@
-// Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", { value: true });
 var UserController = /** @class */ (function () {
     function UserController() {
     }
